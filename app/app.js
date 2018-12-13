35f2c78f748713599e6e0e62af7cb3bb2d294ded
nirch <nirh2@yahoo.com>
2018-12-13 10:18:18 +0200

    Initial commit - identical to recipe-book-v3

---
+++ app/app.js
@@ -0,0 +1,22 @@
+
+var app = angular.module("recipeBookApp", ["ngRoute", "ngImageInputWithPreview"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/home/home.html"
+    }).when("/login", {
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
+    }).when("/signup", {
+
+    }).when("/recipes", {
+        templateUrl: "app/recipes/recipeGallery.html",
+        controller: "recipeGalleryCtrl"
+    }).when("/new" , {
+        templateUrl: "app/recipes/newRecipe.html",
+        controller: "newRecipeCtrl"
+    }).when("/recipe/:id" , {
+
+    })
+})
